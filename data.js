fab4efc6f708f9c2c547b4c0cce16622e0e218c6
islamicdata <159684698+islamicdata@users.noreply.github.com>
2024-02-11 03:10:38 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+const quotesData = {
+    "quotes": [
+        {
+            "verse-number": "(temp:temp)",
+            "arabic-text": "بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ",
+            "end": "",
+            "translation": "In the name of Allah, the Most Gracious, the Most Merciful.",
+            "mp3": "1.mp3"
+        },
+        {
+            "verse-number": "(2:255)",
+            "arabic-text": "اَللّٰهُ لَاۤ اِلٰهَ اِلَّا هُوَ ۚ— اَلْحَیُّ الْقَیُّوْمُ ۚ۬— لَا تَاْخُذُهٗ سِنَةٌ وَّلَا نَوْمٌ ۚ— لَهٗ مَا فِی السَّمٰوٰتِ وَمَا فِی الْاَرْضِ ۚ— مَنْ ذَا الَّذِیْ یَشْفَعُ عِنْدَهٗۤ اِلَّا بِاِذْنِهٖ ۚ— یَعْلَمُ مَا بَیْنَ اَیْدِیْهِمْ وَمَا خَلْفَهُمْ ۚ— وَلَا یُحِیْطُوْنَ بِشَیْءٍ مِّنْ عِلْمِهٖۤ اِلَّا بِمَا شَآءَ ۚ— وَسِعَ كُرْسِیُّهُ السَّمٰوٰتِ وَالْاَرْضَ ۚ— وَلَا یَـُٔوْدُهٗ حِفْظُهُمَا ۚ— وَهُوَ الْعَلِیُّ الْعَظِیْمُ",
+            "end": "۟",
+            "translation": "Allah! La ilaha illa Huwa (none has the right to be worshipped but He), the Ever Living, the One Who sustains and protects all that exists. Neither slumber, nor sleep overtake Him. To Him belongs whatever is in the heavens and whatever is on earth. Who is he that can intercede with Him except with His Permission? He knows what happens to them (His creatures) in this world, and what will happen to them in the Hereafter. And they will never compass anything of His Knowledge except that which He wills. His Kursi extends over the heavens and the earth, and He feels no fatigue in guarding and preserving them. And He is the Most High, the Most Great. [This Verse 2:255 is called Ayat-ul-Kursi]",
+            "mp3": "2.mp3"
+        },
+        {
+            "verse-number": "(3:19)",
+            "arabic-text": "إِنَّ الدِّينَ عِندَ اللَّهِ الْإِسْلَامُ ۗ وَمَا اخْتَلَفَ الَّذِينَ أُوتُوا الْكِتَابَ إِلَّا مِن بَعْدِ مَا جَاءَهُمُ الْعِلْمُ بَغْيًا بَيْنَهُمْ ۗ وَمَن يَكْفُرْ بِآيَاتِ اللَّهِ فَإِنَّ اللَّهَ سَرِيعُ الْحِسَابِ",
+            "end": "۟",
+            "translation": "Indeed, the religion in the sight of Allah is Islam. And those who were given the Scripture did not differ except after knowledge had come to them - out of jealous animosity between themselves. And whoever disbelieves in the verses of Allah, then indeed, Allah is swift in [taking] account.",
+            "mp3": "1.mp3"
+        }
+        // Add more quotes here
+    ]
+};
